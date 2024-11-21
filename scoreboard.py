5b6d41253348e9f36ec96c2ea0392d0d0b07f62d
Dipesh <dipeshluitel40@gmail.com>
2024-11-21 09:12:44 +0545

    highscore will be saved even when game is closed

--- scoreboard.py
+++ scoreboard.py
@@ -5,7 +5,9 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.highscore = 0
+        with open("data.txt", mode="r") as file:
+            past_high_score = int(file.read())
+        self.highscore = past_high_score
         self.score = 0
         self.hideturtle()
         self.penup()
@@ -15,13 +17,13 @@ class Scoreboard(Turtle):
         self.goto(0, 270)
         self.color("white")
         self.penup()
-        self.write(f"SCORE : {self.score}\t\tHighScore : {self.highscore} ", move=False, align='center', font=('Arial', 20, 'normal'))
+        self.write(f"SCORE : {self.score}\t\tHighScore : {self.highscore} ", move=False, align='center',
+                   font=('Arial', 20, 'normal'))
 
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.highscore))
         self.score = 0
         self.display_score()
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("GAME OVER", move=False, align='center', font=('arial', 60, 'bold'))
